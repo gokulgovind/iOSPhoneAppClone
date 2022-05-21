78c9d492f9242551aa3a4eff4ea9fbc745239898
Gokul <gokul.g@external.nexigroup.com>
2022-05-21 21:11:14 +0530

    UI issue on favourite

--- iOSPhoneAppClone/View/FavouriteView.swift
+++ iOSPhoneAppClone/View/FavouriteView.swift
@@ -73,28 +73,29 @@ struct FavouriteRow: View {
             
             
             
-            //            VStack(alignment: .leading, spacing: 3) {
-            Text(contact.contactName)
-                .font(Font.headline)
-                .fontWeight(.semibold)
-            HStack {
-                Image(systemName:"phone.fill")
-                    .resizable()
-                    .frame(width: 12, height: 12, alignment: .center)
-                    .foregroundColor(Color.gray)
-                Text(contact.contactLabel)
-                    .foregroundColor(Color.secondary)
+            VStack(alignment: .leading, spacing: 3) {
+                Text(contact.contactName)
+                    .font(Font.headline)
+                    .fontWeight(.semibold)
+                HStack {
+                    Image(systemName:"phone.fill")
+                        .resizable()
+                        .frame(width: 12, height: 12, alignment: .center)
+                        .foregroundColor(Color.gray)
+                    Text(contact.contactLabel)
+                        .foregroundColor(Color.secondary)
+                }
             }
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Image(systemName:"info.circle")
+                    .resizable()
+                    .frame(width: 22, height: 22, alignment: .center)
+                    .foregroundColor(Color.accentColor)
+            })
         }
-        Spacer()
-        Button(action: {
-            
-        }, label: {
-            Image(systemName:"info.circle")
-                .resizable()
-                .frame(width: 22, height: 22, alignment: .center)
-                .foregroundColor(Color.accentColor)
-        })
     }
+    
 }
-
